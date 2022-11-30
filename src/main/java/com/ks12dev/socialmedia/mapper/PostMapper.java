47b49b58ca5990d08602d5d650e42c44793bb5c5
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2022-11-30 23:55:09 +0530

    Implemented Vote (up/downvote) APIs

--- src/main/java/com/ks12dev/socialmedia/mapper/PostMapper.java
+++ src/main/java/com/ks12dev/socialmedia/mapper/PostMapper.java
@@ -1,6 +1,7 @@
 package com.ks12dev.socialmedia.mapper;
 
 //import com.github.marlonlom.utilities.timeago.TimeAgo;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.ks12dev.socialmedia.dto.PostRequest;
 import com.ks12dev.socialmedia.dto.PostResponse;
 import com.ks12dev.socialmedia.model.*;
@@ -37,33 +38,37 @@ public abstract class PostMapper {
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.username")
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    @Mapping(target = "upVote", expression = "java(isPostUpVoted(post))")
+    @Mapping(target = "downVote", expression = "java(isPostDownVoted(post))")
     public abstract PostResponse mapToDto(Post post);
 
     Integer commentCount(Post post) {
         return commentRepository.findByPost(post).size();
     }
 
-//    String getDuration(Post post) {
-//        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
-//    }
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 
-//    boolean isPostUpVoted(Post post) {
-//        return checkVoteType(post, UPVOTE);
-//    }
-//
-//    boolean isPostDownVoted(Post post) {
-//        return checkVoteType(post, DOWNVOTE);
-//    }
+    boolean isPostUpVoted(Post post) {
+        return checkVoteType(post, UPVOTE);
+    }
 
-//    private boolean checkVoteType(Post post, VoteType voteType) {
-//        if (authService.isLoggedIn()) {
-//            Optional<Vote> voteForPostByUser =
-//                    voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post,
-//                            authService.getCurrentUser());
-//            return voteForPostByUser.filter(vote -> vote.getVoteType().equals(voteType))
-//                    .isPresent();
-//        }
-//        return false;
-//    }
+    boolean isPostDownVoted(Post post) {
+        return checkVoteType(post, DOWNVOTE);
+    }
+
+    private boolean checkVoteType(Post post, VoteType voteType) {
+        if (authService.isLoggedIn()) {
+            Optional<Vote> voteForPostByUser =
+                    voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post,
+                            authService.getCurrentUser());
+            return voteForPostByUser.filter(vote -> vote.getVoteType().equals(voteType))
+                    .isPresent();
+        }
+        return false;
+    }
 
 }
